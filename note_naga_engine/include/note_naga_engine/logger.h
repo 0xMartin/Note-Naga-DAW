fd3f005fefcf22ef0976a162df61577a5435cd4a
0xMartin <martin.krcma1@gmail.com>
2025-07-22 02:48:15 +0200

    update

--- note_naga_engine/include/note_naga_engine/logger.h
+++ note_naga_engine/include/note_naga_engine/logger.h
@@ -1,11 +1,28 @@
 #pragma once
 
+#include <note_naga_engine/note_naga_api.h>
+
 #include <fstream>
 #include <string>
 #include <mutex>
 
-class NoteNagaLogger {
+/**
+ * @brief A simple logger for the Note Naga Engine. Logs messages to file and console.
+ * 
+ * @example Usage:
+ * NOTE_NAGA_LOG_INFO("This is an info message.");
+ * NOTE_NAGA_LOG_WARNING("This is a warning message.");
+ * NOTE_NAGA_LOG_ERROR("This is an error message.");
+ */
+class NOTE_NAGA_ENGINE_API NoteNagaLogger {
 public:
+    /**
+     * @brief Log levels for the Note Naga Engine logger.
+     * These levels can be used to categorize log messages.
+     * - INFO: General information messages.
+     * - WARNING: Messages indicating potential issues.
+     * - ERROR: Messages indicating errors that need attention.
+     */
     enum class Level { INFO, WARNING, ERROR };
 
     static NoteNagaLogger& instance() {
@@ -13,6 +30,12 @@ public:
         return inst;
     }
 
+    /**
+     * @brief Logs a message with the specified level and file name.
+     * @param level The log level (INFO, WARNING, ERROR).
+     * @param msg The message to log.
+     * @param file The source file name where the log is called.
+     */
     void log(Level level, const std::string& msg, const char* file);
 
     // Convenience methods
